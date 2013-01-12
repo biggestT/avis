20632c9f1b380c085f210be87061ac9b1526a098
Tor Nilsson Ohrn <tornilssonohrn@gmail.com>
2013-01-12 13:42:24 +0100

    included rotating webGL stick to start with

--- js/glViewer.js
+++ js/glViewer.js
@@ -1,58 +1,32 @@
 
 // Matrices
-var mvMatrix = 0;
-var pMatrix = 0;
+var model = 0;
+var view = 0;
+var projection = 0;
 
+var modelStack = [];
 
-GlViewer = function(canvasId) {
-	var triangleVertexPositionBuffer;
-  var squareVertexPositionBuffer;
- 
-
-  // Background color
-  this.backgroundColor = [120.0 / 255.0,
-                         169.0 / 255.0,
-                         100.0 / 255.0,
-                         1.0];
-  // Foreground color
-  this.foregroundColor = [63.0 / 255.0,
-                         39.0 / 255.0,
-                         0.0 / 255.0,
-                         1.0];
-
-	this.initGL(canvasId);
-
-	this.drawScene();
-	}
-
-GlViewer.prototype.initGL = function(canvasId) {
-	mvMatrix = mat4.create();
-	pMatrix = mat4.create();
-	 var backgroundColor = this.backgroundColor;
-
-	this.canvas = document.getElementById(canvasId);
-	console.log(canvasId)
-	this.gl = this.canvas.getContext("experimental-webgl");
-	this.gl.viewportWidth = this.canvas.width;
-	this.gl.viewportHeight = this.canvas.height;
-	// Cameracontroller calls cameracontroller.js 
-	this.cameraController = new CameraController(this.canvas);
-	this.cameraController.xRot = -30; //-55;
-	this.cameraController.yRot = 0; 
-	 
-	this.initShaders();
-	this.initBuffers();
-	
-	this.gl.clearColor(backgroundColor[0], backgroundColor[1], backgroundColor[2], backgroundColor[3]);
-	this.gl.enable(this.gl.DEPTH_TEST);
-
-	mvMatrix = mat4.create();
-  pMatrix = mat4.create();
-
-	console.log(" initGL  ");
-	console.log(mvMatrix);           
-
+function degToRad(degrees) {
+        return degrees * Math.PI / 180;
 }
+
+function pushMatrix(stack, matrix) {
+	var copy = mat4.create();
+	mat4.set(matrix, copy);
+	stack.push(copy);
+	return stack;
+}
+
+function popMatrix(stack) {
+	var matrix;
+	if (stack.length == 0) {
+	  throw "Invalid popMatrix!";
+	}
+	matrix = stack.pop();
+	return matrix;
+}
+
+
 
 function getShader(gl, id) {
 	var shaderScript = document.getElementById(id);
@@ -88,10 +62,9 @@ function getShader(gl, id) {
 	}
 
 	return shader;
-	}
+}
 
-GlViewer.prototype.initShaders = function () {
-	var gl = this.gl;
+function initShaders (gl) {
 	console.log(gl);
 	var fragmentShader = getShader(gl, "shader-fs");
 	var vertexShader = getShader(gl, "shader-vs");
@@ -110,47 +83,78 @@ GlViewer.prototype.initShaders = function () {
 	shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
 	gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
 
-	shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-	shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
-	this.shaderProgram = shaderProgram;
+	shaderProgram.projection = gl.getUniformLocation(shaderProgram, "projection");
+	shaderProgram.model = gl.getUniformLocation(shaderProgram, "model");
+	shaderProgram.view = gl.getUniformLocation(shaderProgram, "view");
+	return shaderProgram;
 }
 
 
 
- GlViewer.prototype.setMatrixUniforms = function () {
-	var gl = this.gl;
-	
-  console.log(pMatrix);
+ function setMatrixUniforms (gl, shaderProgram) {
 
-	gl.uniformMatrix4fv(this.shaderProgram.pMatrixUniform, false, pMatrix);
-	gl.uniformMatrix4fv(this.shaderProgram.mvMatrixUniform, false, mvMatrix);
+	gl.uniformMatrix4fv(shaderProgram.projection, false, projection);
+	gl.uniformMatrix4fv(shaderProgram.model, false, model);
+	gl.uniformMatrix4fv(shaderProgram.view, false, view);
+
 }
 
-GlViewer.prototype.initBuffers = function () {
-	var gl = this.gl;
-	triangleVertexPositionBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-	var vertices = [
-		0.0,  1.0,  0.0,
-		-1.0, -1.0,  0.0,
-		1.0, -1.0,  0.0
-	];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	triangleVertexPositionBuffer.itemSize = 3;
-	triangleVertexPositionBuffer.numItems = 3;
-
+function initBuffers (gl) {
 	squareVertexPositionBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-	vertices = [
-		1.0,  1.0,  0.0,
-		-1.0,  1.0,  0.0,
-		1.0, -1.0,  0.0,
-		-1.0, -1.0,  0.0
-	];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	squareVertexPositionBuffer.itemSize = 3;
-	squareVertexPositionBuffer.numItems = 4;
+  gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+  vertices = [
+       0.0,  0.0,  0.0,
+       1.0,  1.0,  0.0,
+       1.0,  1.0,  0.0,
+       2.0,  7.0,  0.0,
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  squareVertexPositionBuffer.itemSize = 3;
+  squareVertexPositionBuffer.numItems = 4;
 }
+
+GlViewer = function(canvasId) {
+	var triangleVertexPositionBuffer;
+
+  this.backgroundColor = [0.0, 0.0, 0.0, 1.0];
+  this.foregroundColor = [1.0, 1.0, 1.0, 1.0];
+
+	this.initGL(canvasId);
+	console.log(" Gl created ");
+}
+
+GlViewer.prototype.initGL = function(canvasId) {
+	model = mat4.create();
+	view = mat4.create();
+	projection = mat4.create();
+	
+	var backgroundColor = this.backgroundColor;
+
+	this.canvas = document.getElementById(canvasId);
+
+	this.gl = this.canvas.getContext("experimental-webgl");
+	this.gl.viewportWidth = this.canvas.width;
+	this.gl.viewportHeight = this.canvas.height;
+
+	// Cameracontroller calls cameracontroller.js 
+	this.cameraController = new CameraController(this.canvas);
+	this.cameraController.xRot = -30; //-55;
+	this.cameraController.yRot = 0; 
+	 
+	this.shaderProgram = initShaders(this.gl);
+	initBuffers(this.gl, this.shaderProgram);
+	
+	this.gl.clearColor(backgroundColor[0], backgroundColor[1], backgroundColor[2], backgroundColor[3]);
+	this.gl.enable(this.gl.DEPTH_TEST);
+
+	model = mat4.create();
+  projection = mat4.create();
+
+	console.log(" initGL  ");
+	// console.log(model);           
+
+}
+
 
 GlViewer.prototype.drawScene = function () {
 	var gl = this.gl;
@@ -158,29 +162,46 @@ GlViewer.prototype.drawScene = function () {
 	gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
-	console.log("pMatrix: ");
-	console.log(gl.viewportWidth);
-	mat4.identity(mvMatrix);
-	// mat4.rotate(mvMatrix, this.cameraController.xRot, 1, 0, 0);
-	// mat4.rotate(mvMatrix, this.cameraController.yRot, 0, 1, 0);
-	// console.log(this.cameraController);
+	// Set up the model, view and projection matrices
+  // mat4.identity(projection);
+  mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, projection);
 
-	console.log("vMatrix: ");
-	console.log(mvMatrix);
-	mat4.translate(mvMatrix, [-1.5, 0.0, -10.0]);
-	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	this.setMatrixUniforms();
-	gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
+  mat4.identity(view);
+  
 
+  // Add in camera controller's rotation
+  mat4.identity(model);
+  mat4.translate(model, [-2.0, 0.0, -50.0]);
+  
+  mat4.rotate(model, this.cameraController.xRot, [1, 0, 0]);
+  mat4.rotate(model, this.cameraController.yRot, [0, 1, 0]);
 
-	mat4.translate(mvMatrix, [3.0, 0.0, -5.0]);
-	gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	this.setMatrixUniforms();
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
+	// pushMatrix(modelStack, model);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    setMatrixUniforms(gl, shaderProgram);
+    gl.drawArrays(gl.LINES, 0, squareVertexPositionBuffer.numItems);
+
+	// model = popMatrix(modelStack);
+	
 }
+
+var lastTime = 0;
+
+GlViewer.prototype.animate = function () {
+  var timeNow = new Date().getTime();
+  if (lastTime != 0) {
+    var elapsed = timeNow - lastTime;
+
+    this.rTri += (90 * elapsed) / 1000.0;
+    this.rSquare += (75 * elapsed) / 1000.0;
+  }
+	lastTime = timeNow;
+}
+
+
   
 GlViewer.prototype.update = function(amplitudes) {
 	// this.drawScene();
